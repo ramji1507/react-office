cf05b6a0f17a8fa2804f1ca213554940e8e0e4f9
unknown <ramji.codingkart@gmail.com>
2019-12-27 16:29:36 +0530

    react deploy

---
+++ build/precache-manifest.e78f9a902f09328d4ae3ba62af3f8a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac6ec51b94a8526d76895c935b0615a6",
+    "url": "/react-office/index.html"
+  },
+  {
+    "revision": "25b43e57a138b6a86c64",
+    "url": "/react-office/static/css/main.f1933cde.chunk.css"
+  },
+  {
+    "revision": "10f661ddc01d4c5592fd",
+    "url": "/react-office/static/js/2.f3886b48.chunk.js"
+  },
+  {
+    "revision": "a5c40d69aa7e6878f96af19885556f2d",
+    "url": "/react-office/static/js/2.f3886b48.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25b43e57a138b6a86c64",
+    "url": "/react-office/static/js/main.d72624a6.chunk.js"
+  },
+  {
+    "revision": "ddc8a85a671021bf433b",
+    "url": "/react-office/static/js/runtime-main.0519acf1.js"
+  }
+]);
